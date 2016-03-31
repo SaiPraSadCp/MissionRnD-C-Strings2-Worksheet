fe98283b7693ed47d03720d640b894fd2da6eb78
SaiPraSadCp <SaiPraSadCp@missionrnd.com>
2016-03-31 23:38:53 +0530

    Submit Commit

--- spec/getFrequencyofWordSpec.cpp
+++ spec/getFrequencyofWordSpec.cpp
@@ -1,29 +1,35 @@
-
-#include "stdafx.h"
-#include "CppUnitTest.h"
-#include "../src/getFrequencyofWord.cpp"
-#include <stddef.h>
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
-namespace spec
-{
-	TEST_CLASS(getFrequencyofWordSpec)
+# include "stdafx.h"
+#include <stdlib.h>
+int count_word_in_str_way_1(char *str, char *word){
+	int index, count = 0, windex, flag, tindex = 0;
+	for (index = 0; str[index]; index++)
 	{
-	public:
-
-		TEST_METHOD(NormalStringFrequency)
+		if (word[0] == str[index])
 		{
-			char sentences[][100] = { "abababa", "he he", "ab cd ab cd", "    ", "1234 aaaaaaa", "asd324", "yzqwer 23425", "a" };
-			char words[][100] = { "ab", "he", "ab cd", " ", "aa", "asd324", "x", "a" };
-			int counts[100] = { 3,2,2,4,6,1,0};
-			int sc = 7;
-			int si = 0;
-			for (si = 0; si < sc; si++){
-				int temp_c = count_word_in_str_way_1(sentences[si],words[si]);
-				Assert::AreEqual(temp_c,counts[si], L"Error in NormalStringLastword", LINE_INFO());
-				
+			windex = 1;
+			tindex = index + 1;
+			flag = 0;
+			while (word[windex])
+			{
+				if (word[windex] == str[tindex])
+				{
+					windex++;
+					tindex++;
+					continue;
+				}
+				else
+				{
+					flag = 1;
+					break;
+				}
 			}
-
+			if (flag == 0)
+				count++;
 		}
-	};
+	}
+	return count;
+}
+
+int count_word_int_str_way_2_recursion(char *str, char *word){
+	return 0;
 }
